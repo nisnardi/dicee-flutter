2eca96cc8b05574c1d51c4b137d4345baad4fcad
Nicolas Isnardi <nicolasisnardi@gmail.com>
2020-01-02 16:57:17 -0600

    finished flutter course unit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
+  MaterialColor backgroundColor = Colors.red;
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: backgroundColor,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: backgroundColor,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+int getRandomNumber(int limit) {
+  return Random().nextInt(limit) + 1;
+}
+
+Image getImageAsset(int number) {
+  return Image.asset('images/dice$number.png');
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber = getRandomNumber(6);
+  int _rightDiceNumber = getRandomNumber(6);
+
+  void changeDiceFace() {
+    setState(() {
+      _rightDiceNumber = getRandomNumber(6);
+      _leftDiceNumber = getRandomNumber(6);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+// () {} anonymous function
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: getImageAsset(_leftDiceNumber),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: getImageAsset(_rightDiceNumber),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
